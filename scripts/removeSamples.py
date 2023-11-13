b25c6e8492fc1c6e7bbc2508830f3c6a455d0001
Nicole Kramer <nekramer@live.unc.edu>
2023-11-13 16:31:39 -0500

    Updating sample removal to skip files that are not found

--- scripts/removeSamples.py
+++ scripts/removeSamples.py
@@ -1,23 +1,33 @@
 import sys
 import pandas as pd
 import difflib
+import os
 
 # Read in samples to remove
 removeSamples = pd.read_csv(sys.argv[1], sep = " ")
 
 removePLINK = []
 for row in removeSamples.itertuples():
-    # Grab fam file for batch
-    famFile = pd.read_csv('output/binary/' + row.Batch + '.fam', sep = " ", header = None)
 
-    # Match donor to 2nd column
-    donorID = difflib.get_close_matches(row.Donor, famFile.iloc[:,1], 1, cutoff = 0.4)[0]
+    if os.path.isfile('output/binary/' + row.Batch + '.fam') == False:
+        print('Files for batch ' + row.Batch + ' not found. Skipping...')
+    else:
 
-    # Select FID and within-family id columns
-    colIDs = famFile.loc[(famFile[1] == donorID)][[0, 1]]
+        famFile = pd.read_csv('output/binary/' + row.Batch + '.fam', sep = " ", header = None)
 
-    removePLINK.append(colIDs)
+        # Match donor to 2nd column
+        donorID = difflib.get_close_matches(row.Donor, famFile.iloc[:,1], 1, cutoff = 0.4)[0]
 
-removePLINK_df = pd.concat(removePLINK)
+        # Select FID and within-family id columns
+        colIDs = famFile.loc[(famFile[1] == donorID)][[0, 1]]
 
-removePLINK_df.to_csv("remove.plink", sep = " ", header = False, index = False)
+        removePLINK.append(colIDs)
+
+if len(removePLINK) > 0:
+    removePLINK_df = pd.concat(removePLINK)
+    removePLINK_df.to_csv("remove.plink", sep = " ", header = False, index = False)
+else:
+    print('No samples to remove.')
+    with open('remove.plink', 'w') as f:
+        pass    
+
