c7c6bbff91e509dac5f8c08cdc646857a97134c5
Nicole Kramer <nekramer@live.unc.edu>
2024-04-22 16:36:17 -0400

    adjust sample removal to exact matching

--- scripts/removeSamples.py
+++ scripts/removeSamples.py
@@ -15,11 +15,8 @@ for row in removeSamples.itertuples():
 
         famFile = pd.read_csv('output/binary/' + row.Batch + '.fam', sep = " ", header = None)
 
-        # Match donor to 2nd column
-        donorID = difflib.get_close_matches(row.Donor, famFile.iloc[:,1], 1, cutoff = 0.4)[0]
-
         # Select FID and within-family id columns
-        colIDs = famFile.loc[(famFile[1] == donorID)][[0, 1]]
+        colIDs = famFile.loc[(famFile[1] == row.Donor)][[0, 1]]
 
         removePLINK.append(colIDs)
 
